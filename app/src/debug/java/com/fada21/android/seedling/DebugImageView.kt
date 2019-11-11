f7cedde44b119136daed14f4cb4e495a27a117b2
Łukasz Czaplicki <lukasz.czaplicki@gmail.com>
2019-11-11 17:46:24 +0000

    improve debug image view

--- app/src/debug/java/com/fada21/android/seedling/DebugImageView.kt
+++ app/src/debug/java/com/fada21/android/seedling/DebugImageView.kt
@@ -1,5 +1,6 @@
 package com.fada21.android.seedling
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
@@ -13,12 +14,33 @@ class DebugImageView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
-    var onUpdateMeasure: (Long) -> Unit = {}
+    var onUpdateMeasure: (
+        onDrawDurationNano: Long,
+        onDrawDurationAverage: Float
+    ) -> Unit = { _, _ -> }
 
+    private var keepRedrawing = false
+    private val avgQueue = ArrayList<Long>()
+    private val averageNano get() = avgQueue.average()
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        setOnClickListener { invalidate() }
+        setOnLongClickListener {
+            keepRedrawing = !keepRedrawing
+            invalidate()
+            true
+        }
+    }
+
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas?) {
-        onUpdateMeasure(measureNanoTime {
+        val nanoTime = measureNanoTime {
             super.onDraw(canvas)
-        })
+        }
+        avgQueue += nanoTime
+        onUpdateMeasure(nanoTime, averageNano.toLong() / 1_000L / 1000.0F)
+        if (keepRedrawing) invalidate()
     }
 
 }
